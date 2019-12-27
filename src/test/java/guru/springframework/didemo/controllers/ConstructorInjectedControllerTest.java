6f9501ae9b3e6274983846a8727cd6f6f067248b
kitamarekweb <kitamarekweb@gmail.com>
2019-12-27 11:09:10 +0100

    manual di update

---
+++ src/test/java/guru/springframework/didemo/controllers/ConstructorInjectedControllerTest.java
@@ -0,0 +1,23 @@
+package guru.springframework.didemo.controllers;
+
+import guru.springframework.didemo.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ConstructorInjectedControllerTest {
+
+    private ConstructorInjectedController constructorInjectedController;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        this.constructorInjectedController = new ConstructorInjectedController(new GreetingServiceImpl());
+    }
+
+    @Test
+    public void testGreeting() {
+        assertEquals(GreetingServiceImpl.HELLO_GURUS, constructorInjectedController.sayHello());
+    }
+
+}
