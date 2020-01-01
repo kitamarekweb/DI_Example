a10a169bc8b6d2daf8754e0f3b2f086b1cc4a1e0
kitamarekweb <kitamarekweb@gmail.com>
2020-01-01 14:05:56 +0100

    t-95. spring environment properties. getting properties from system environment properties

--- src/main/java/guru/springframework/didemo/config/PropertyConfig.java
+++ src/main/java/guru/springframework/didemo/config/PropertyConfig.java
@@ -1,17 +1,23 @@
 package guru.springframework.didemo.config;
 
 import guru.springframework.didemo.examplebeans.FakeDataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
 
-    @Value("${guru.user}")
+    @Autowired
+    Environment env;
+
+    @Value("${guru.username}")
     String user;
 
     @Value("${guru.password}")
@@ -23,7 +29,8 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(user);
+        fakeDataSource.setUser(env.getProperty("USERNAME"));
+//        fakeDataSource.setUser(user);
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
